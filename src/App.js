82af1b29ed955d5e63780233e6cc363614cb1c65
Ryo Murakami <ryo@Ryo-MacBook-Pro.local>
2023-07-01 09:46:46 +0900

    update map

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import { GoogleMap, LoadScript, MarkerF } from '@react-google-maps/api';
 import './App.css';
 
-const container = {
-  width: "75%",
-  height: "500px"
+const containerStyle = {
+  height: "100vh",
+  width: "100%",
+  // height: "500px"
 };
 
-const position = {
+const center = {
+  lat: 35.681,
+  lng: 139.781
+}
+
+const positionTokyoStation = {
   lat: 35.680959106959,
   lng: 139.76730676352
 };
@@ -14,11 +20,11 @@ const position = {
 function App() {
   return (
     <>
-      <h2>React & Google Map</h2>
+      {/* <h2>React & Google Map</h2> */}
       <div className='wrap'>
         <LoadScript googleMapsApiKey={process.env.REACT_APP_GOOGLE_MAP_API_KEY}>
-          <GoogleMap mapContainerStyle={container} center={position} zoom={15}>
-            <MarkerF position={position} label={"東京駅です"} />
+          <GoogleMap mapContainerStyle={containerStyle} center={center} zoom={15}>
+            <MarkerF position={positionTokyoStation} label={"東京駅です"} />
           </GoogleMap>
         </LoadScript>
       </div>
